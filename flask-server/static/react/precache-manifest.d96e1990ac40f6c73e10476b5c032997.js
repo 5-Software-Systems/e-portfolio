cdbb2811565342bf670689ff79f3a2506c79c3e0
Marcus Litterick <marcus.litterick@gmail.com>
2020-08-19 15:15:46 +1000

    added semantic-ui-react

---
+++ flask-server/static/react/precache-manifest.d96e1990ac40f6c73e10476b5c032997.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e06d67c5c8520f56d7fb9408c01988",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "a8d71a27684f27c84b94",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d297e85a49c7257126a",
+    "url": "/static/react/js/2.1cb910e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.1cb910e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8d71a27684f27c84b94",
+    "url": "/static/react/js/main.a2d89f27.chunk.js"
+  },
+  {
+    "revision": "3d13f19517ec8b4b25ec",
+    "url": "/static/react/js/runtime-main.bcf44a74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
