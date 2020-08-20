9828976d486849edcc0bd3d27a721cae429246a9
Marcus Litterick <marcus.litterick@gmail.com>
2020-08-20 17:55:37 +1000

    create user ui with link to backend

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import Signup from './containers/Signup';
+
 
 function App() {
     const [test, setTest] = useState({"test": "API not running"});
@@ -16,6 +18,7 @@ function App() {
         <div className="App">
             <header>This is a test</header>
             <span>API return: {test.response}</span>
+            <Signup />
         </div>
     );
 }
---
+++ app/src/containers/Signup.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import {
+    FormGroup,
+    FormControl,
+    FormLabel,
+    Button,
+} from "react-bootstrap";
+import './Signup.css'
+
+
+function useFormFields(initialState) {
+    const [fields, setValues] = useState(initialState);
+
+    return [
+        fields,
+        function(event) {
+            setValues({
+                ...fields,
+                [event.target.id]: event.target.value
+            });
+        }
+    ];
+}
+
+export default function Signup() {
+    const [fields, handleFieldChange] = useFormFields({
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+    function validateForm() {
+        return (
+            fields.email.length > 0 &&
+            fields.password.length > 0 &&
+            fields.confirmPassword === fields.password
+        );
+    }
+
+    async function handleSubmit() {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({password: fields.password, email: fields.email})
+        };
+        const response = await fetch('/api/user/', requestOptions);
+        const data = await response.json();
+        console.log(data);
+    }
+
+    function renderForm() {
+        return (
+            <form>
+                <FormGroup controlId="email">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl 
+                        type="email"
+                        values = {fields.email}
+                        onChange={handleFieldChange}
+                    />
+                </FormGroup>
+                <FormGroup controlId="password">
+                    <FormLabel>Password</FormLabel>
+                    <FormControl
+                        type="password"
+                        value={fields.password}
+                        onChange={handleFieldChange}
+                />
+                </FormGroup>
+                <FormGroup controlId="confirmPassword">
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl
+                        type="password"
+                        onChange={handleFieldChange}
+                        value={fields.confirmPassword}
+                    />
+                </FormGroup>
+                <SubmitButton />
+            </form>
+        );
+    }
+    function SubmitButton() {
+        const [isLoading, setLoading] = useState(false);
+    
+        useEffect(() => {
+            if (isLoading && validateForm()) {
+                handleSubmit().then(() => {
+                    setLoading(false);
+                });
+            }
+        }, [isLoading]);
+    
+        const handleClick = () => setLoading(true);
+    
+        return (
+            <Button
+                variant="primary"
+                disabled={isLoading}
+                onClick={!isLoading ? handleClick : null}
+                type="submit"
+            >
+                {isLoading ? 'Loading...' : 'Submit'}
+            </Button>
+        );
+    }
+    return (
+        <div className="Signup">
+            {renderForm()}
+        </div>
+    );
+}
