45c9dd4c3882a7eae6c1dc53cf49859450dd1838
csagar <calvinshinji@gmail.com>
2020-08-30 17:52:05 +0900

    hello gamers

--- app/src/index.js
+++ app/src/index.js
@@ -4,20 +4,15 @@ import LandingContent from './components/Landing';
 import LandingButtons from './components/LandingButtons';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import EPortfolio from "./components/EPortfolio";
 
 ReactDOM.render(
   <React.StrictMode>
-      <LandingButtons />
+      <EPortfolio />
   </React.StrictMode>,
-  document.getElementById('nav_right')
+  document.getElementById('root')
 );
 
-ReactDOM.render(
-  <React.StrictMode>
-      <LandingContent />
-  </React.StrictMode>,
-  document.getElementById('content')
-);
 
 
 // If you want your app to work offline and load faster, you can change
