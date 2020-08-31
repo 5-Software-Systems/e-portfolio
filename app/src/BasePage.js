c15aefe87ee4d1c591170a1c4a91b7e0b92bb80b
csagar <calvinshinji@gmail.com>
2020-09-01 00:41:19 +0900

    mapped profile previews

--- app/src/BasePage.js
+++ app/src/BasePage.js
@@ -4,21 +4,33 @@ import AddPortfolio from "./containers/AddPortfolio";
 
 function BasePage(){
 
-    //test user
-    var userID = "1a933523-99e9-4ebb-86f9-aff4e920763e"
+    //grab user
+    var userID = "737bad35-39f8-4ae1-978a-6f60bd32ae56"
 
     const [user, setUser] = useState([]);
 
-    useEffect( () =>{
-        fetchUser();
-    }, [])
-
     const fetchUser = async () => {
-        const data = await fetch('/api/user/'.concat(userID));
+        const data = await fetch('/api/user/'+ userID);
         const user = await data.json();
         console.log(user.user);
         setUser(user.user);
     }
+
+    //grab profiles
+    const [profiles, setProfiles] = useState([]);
+
+    const fetchProfiles = async() => {
+        const data = await fetch('/api/user/' + userID + '/portfolio');
+        const profile = await data.json();
+        console.log(profile.portfolios);
+        setProfiles(profile.portfolios);
+    }
+
+    //store db
+    useEffect( () =>{
+        fetchUser();
+        fetchProfiles();
+    }, [])
 
     return(
         <div>
@@ -27,6 +39,9 @@ function BasePage(){
             </div>
             <div className="basepage">
                 < AddPortfolio /> 
+                {profiles.map(profile =>(
+                    < EPortfolioPreview name={profile.title} date={profile.public_id}/>
+                ))}
                 < EPortfolioPreview name={"du ma"} date={"today 24:01 XD"} img={process.env.PUBLIC_URL + '/images/foggers.png'}/>
                 < EPortfolioPreview name={"Mày chửi thế cái con cặc du nói chuyện vô văn hóa,mày coi chừng mày chết với tao nha mậy"} date={"30/10/1999"} img={process.env.PUBLIC_URL + '/images/labtime.png'}/>
             </div>
