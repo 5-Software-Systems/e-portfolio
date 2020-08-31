a350f7c5269bb0460d00a28d8c86c1af281fe893
csagar <calvinshinji@gmail.com>
2020-08-31 16:25:09 +0900

    fetched user

--- app/src/BasePage.js
+++ app/src/BasePage.js
@@ -1,29 +1,34 @@
 import React, {useEffect, useState} from "react";
 import EPortfolioPreview from "./containers/EPortfolioPreview";
+import AddPortfolio from "./containers/AddPortfolio";
 
 function BasePage(){
+
+    //test user
+    var userID = "1a933523-99e9-4ebb-86f9-aff4e920763e"
+
     const [user, setUser] = useState([]);
 
     useEffect( () =>{
-        //fraser's test user
-        fetch('/api/user/520a0b47-5082-4782-8844-029cad3bd7f4').then(response =>
-            response.json().then(data => {
-                console.log(data.user);
-            })
-        );
+        fetchUser();
     }, [])
+
+    const fetchUser = async () => {
+        const data = await fetch('/api/user/'.concat(userID));
+        const user = await data.json();
+        console.log(user.user);
+        setUser(user.user);
+    }
 
     return(
         <div>
-            <div>
-                <h1>{user}'s Base Page</h1>
+            <div className ="title">
+                 <h1>{user.name_first}'s Base Page</h1>
             </div>
             <div className="basepage">
-                < EPortfolioPreview name={"Add new ePortfolio"} date={""} img={"https://pngimg.com/uploads/plus/plus_PNG31.png"}/>
+                < AddPortfolio /> 
                 < EPortfolioPreview name={"du ma"} date={"today 24:01 XD"} img={process.env.PUBLIC_URL + '/images/foggers.png'}/>
                 < EPortfolioPreview name={"Mày chửi thế cái con cặc du nói chuyện vô văn hóa,mày coi chừng mày chết với tao nha mậy"} date={"30/10/1999"} img={process.env.PUBLIC_URL + '/images/labtime.png'}/>
-                < EPortfolioPreview name={"banh mi"} date={"23/10/1999"} img={process.env.PUBLIC_URL + '/images/lofi.jpg'}/>
-                < EPortfolioPreview name={"pho"} date={"25/10/1999"} img={process.env.PUBLIC_URL + '/images/toothbruh.jpg'}/>
             </div>
         </div>
 
