ff6ed88550f4f83ae6d2281ac2ecde1a9a4901a2
csagar <calvinshinji@gmail.com>
2020-08-31 10:44:31 +0900

    add + button

--- app/src/BasePage.js
+++ app/src/BasePage.js
@@ -6,7 +6,7 @@ function BasePage(){
 
     useEffect( () =>{
         //fraser's test user
-        fetch('/api/user/75503149-8af2-4d6a-9b3d-2dcb4738af8f').then(response =>
+        fetch('/api/user/520a0b47-5082-4782-8844-029cad3bd7f4').then(response =>
             response.json().then(data => {
                 console.log(data.user);
             })
@@ -19,6 +19,7 @@ function BasePage(){
                 <h1>{user}'s Base Page</h1>
             </div>
             <div className="basepage">
+                < EPortfolioPreview name={"Add new ePortfolio"} date={""} img={"https://pngimg.com/uploads/plus/plus_PNG31.png"}/>
                 < EPortfolioPreview name={"du ma"} date={"today 24:01 XD"} img={process.env.PUBLIC_URL + '/images/foggers.png'}/>
                 < EPortfolioPreview name={"Mày chửi thế cái con cặc du nói chuyện vô văn hóa,mày coi chừng mày chết với tao nha mậy"} date={"30/10/1999"} img={process.env.PUBLIC_URL + '/images/labtime.png'}/>
                 < EPortfolioPreview name={"banh mi"} date={"23/10/1999"} img={process.env.PUBLIC_URL + '/images/lofi.jpg'}/>
