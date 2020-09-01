fe85e2da48c803263e71c3617721a50dae62643c
csagar <calvinshinji@gmail.com>
2020-09-01 15:08:16 +0900

    removed static previews

--- app/src/BasePage.js
+++ app/src/BasePage.js
@@ -4,7 +4,7 @@ import AddPortfolio from "./containers/AddPortfolio";
 
 function BasePage(){
 
-    //grab user
+    //enter user id here 
     var userID = "737bad35-39f8-4ae1-978a-6f60bd32ae56"
 
     const [user, setUser] = useState([]);
@@ -42,8 +42,6 @@ function BasePage(){
                 {profiles.map(profile =>(
                     < EPortfolioPreview name={profile.title} date={profile.public_id}/>
                 ))}
-                < EPortfolioPreview name={"du ma"} date={"today 24:01 XD"} img={process.env.PUBLIC_URL + '/images/foggers.png'}/>
-                < EPortfolioPreview name={"Mày chửi thế cái con cặc du nói chuyện vô văn hóa,mày coi chừng mày chết với tao nha mậy"} date={"30/10/1999"} img={process.env.PUBLIC_URL + '/images/labtime.png'}/>
             </div>
         </div>
 
