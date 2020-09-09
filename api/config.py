7522eda848561a8cd72aa389cff0dbef367b5698
flangton <flangton@student.unimelb.edu.au>
2020-09-09 16:49:40 +1000

    YEAH BOI

--- api/config.py
+++ api/config.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 # SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "eportfolio.db")
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-SECRET_KEY = "secret"
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "sqlite:///" + os.path.join(BASE_DIR, "eportfolio.db"))
+SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = False
