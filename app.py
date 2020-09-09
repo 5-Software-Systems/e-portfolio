e9bdf81095c852c3e58230a54adc1ef0040a3306
flangton <flangton@student.unimelb.edu.au>
2020-09-09 14:34:04 +1000

    logging
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import logging
+
 from flask import render_template
 
 from api.main import create_app, db
 from api import blueprint
 from api.main.model import *
+from api.test import db as dbtest
 
 
 def build_app():
@@ -13,10 +16,18 @@ def build_app():
     def index():
         return render_template('index.html', token='Hello World')
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     return app
 
 
+dbtest.delete()
+dbtest.create_app()
+
 app = build_app()
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
