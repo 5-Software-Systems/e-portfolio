a18ff23ccbbaa6ef1d0be9e92f4ec754c3de9d0b
csagar <calvinshinji@gmail.com>
2020-09-19 15:57:01 +1000

    added button

--- app/src/components/EPortfolioPreview.js
+++ app/src/components/EPortfolioPreview.js
@@ -4,14 +4,24 @@ import '../styles/BasePage.css';
 
 function EPortfolioPreview(props){
     const link = "/portfolio/" + props.id;
+    const options = ["Edit", "Delete"]
     return(
-        <a href={ link }>
-            <div className="eportfoliopreview content-wrapper">
-                <h3>{props.name}</h3>
-                <p> {props.id} </p>
-                <img src={props.img} alt='' height='150'/>
+        <div className="eportfoliopreview"> 
+            <button type="button" class="button">
+                ···
+            </button>
+            <div> 
+                <a href={ link }>
+                    <div>
+                        <h3>{props.name}</h3>
+                        <p> {props.id} </p>
+                        <img src={props.img} alt='' height='150'/>
+                    </div>
+                </a>
             </div>
-        </a>
+            
+        </div>
+        
     )
 }
 
