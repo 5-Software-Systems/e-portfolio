d011b6ee3ed3ad03a094394b640f2af1b902a78c
Fabrice <fwongkwok@student.unimelb.edu.au>
2020-09-20 10:33:37 -0400

    trying to implement popups

--- app/src/containers/EPortfolio.js
+++ app/src/containers/EPortfolio.js
@@ -68,6 +68,55 @@ export default function EPortfolio() {
         window.location.reload(false);
     }
 
+    const Modal = () => (
+        <Popup
+            trigger={<button className="button"> Open Modal </button>}
+            modal
+            nested
+        >
+            {close => (
+            <div className="modal">
+                <button className="close" onClick={close}>
+                &times;
+                </button>
+                <div className="header"> Modal Title </div>
+                <div className="content">
+                {' '}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+                Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+                delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+                commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+                explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+                </div>
+                <div className="actions">
+                <Popup
+                    trigger={<button className="button"> Trigger </button>}
+                    position="top center"
+                    nested
+                >
+                    <span>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+                    magni omnis delectus nemo, maxime molestiae dolorem numquam
+                    mollitia, voluptate ea, accusamus excepturi deleniti ratione
+                    sapiente! Laudantium, aperiam doloribus. Odit, aut.
+                    </span>
+                </Popup>
+                <button
+                    className="button"
+                    onClick={() => {
+                    console.log('modal closed ');
+                    close();
+                    }}
+                >
+                    close modal
+                </button>
+                </div>
+            </div>
+            )}
+        </Popup>
+      );
 
     return (
         <div className='wholePage'>
@@ -85,12 +134,17 @@ export default function EPortfolio() {
                 {widgets.map(widget =>(
                     < div key={widget.public_id} data-grid={{i: widget.public_id, x: 3, y: 1, w: 1, h: 1}}> 
                         <MotherWidget widget={widget}/>
+                        <div className ='overlay'>
+                            <button className='button' onClick={Modal}>⚙️</button>
+                        </div>
                     </ div>
                 ))}
             </ReactGridLayout>
         </div>
     );
 };
+
+
 
 
 
--- app/src/containers/EPortfolioDemo.js
+++ app/src/containers/EPortfolioDemo.js
@@ -56,6 +56,10 @@ class MyGrid extends React.Component {
 
                 <TextToHTML header="Welcome to My Page!" text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
             </div>
+            <div key="gj" data-grid={{i: 'j', x: 0, y: 0, w: 1, h: 3}}>
+                <iframe width='100%' height='100%' title="embed4" src="https://embed.music.apple.com/au/album/future-nostalgia/1495799403" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <p>hey this is pretty cool</p>
+            </div>
             </ReactGridLayout>
         </div>
       )
