61620c70a56de348b374866e1d9c402cb3bc4d08
Marcus Litterick <mlitterick@student.unimelb.edu.au>
2020-09-22 22:53:06 +1000

    Added sentry error logging

--- api/main/__init__.py
+++ api/main/__init__.py
@@ -1,8 +1,11 @@
 import os
+import sentry_sdk
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from sentry_sdk.integrations.flask import FlaskIntegration
+
 
 """
 To create the initial database, just import the db object from an interactive Python shell and run the 
@@ -11,6 +14,12 @@ SQLAlchemy.create_all() method to create the tables and database:
 db = SQLAlchemy()
 
 flask_bcrypt = Bcrypt()
+
+sentry_sdk.init(
+    dsn="https://abcc2d9c067644ba9caf2af23da31446@o451388.ingest.sentry.io/5437200",
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=1.0
+)
 
 
 def create_app():
