ea676792c3f5d3faf80927cea3a569484ce6725f
Marcus Litterick <mlitterick@student.unimelb.edu.au>
2020-09-24 17:41:33 +1000

    unit tests

--- api/config.py
+++ api/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
+TESTING = False
 # SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "eportfolio.db")
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "sqlite:///" + os.path.join(BASE_DIR, "eportfolio.db"))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
---
+++ tests/test_widget.py
@@ -0,0 +1,23 @@
+import json
+
+def test_create_widget(app, client):
+    pass
+
+
+def test_get_widgets(app, client):
+    pass
+
+
+def test_get_a_widget(app, client):
+    pass
+
+
+def test_patch_widget(app, client):
+    pass
+
+
+def test_delete_widget(app, client):
+    pass
+
+
+
