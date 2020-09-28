1415b426839c4977435a42367e3cb29f25011d31
flangton <flangton@student.unimelb.edu.au>
2020-09-28 16:01:52 +1000

    testing fix

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from api.main.util.funcs import rel_path
 
 DEBUG = True
 TESTING = False
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').format(root=rel_path('', __file__))
-# SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URI', 'sqlite:///:memory:')
+# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').format(root=rel_path('', __file__))
+SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URI')
 SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = False
