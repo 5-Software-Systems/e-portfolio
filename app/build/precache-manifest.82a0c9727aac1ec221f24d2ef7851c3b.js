502a596b9be5ea538d413c160d9c910fd3704bda
flangton <flangton@student.unimelb.edu.au>
2020-09-29 21:05:23 +1000

    deploy

---
+++ app/build/precache-manifest.82a0c9727aac1ec221f24d2ef7851c3b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1905948d8340a18f3e8e49ef6de55d7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28031c97df9c9efda302",
+    "url": "/static/css/2.f1b50ea8.chunk.css"
+  },
+  {
+    "revision": "7408f1f7df886619594d",
+    "url": "/static/css/main.d9161e7c.chunk.css"
+  },
+  {
+    "revision": "28031c97df9c9efda302",
+    "url": "/static/js/2.7d64a8da.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/static/js/2.7d64a8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7408f1f7df886619594d",
+    "url": "/static/js/main.4716474c.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  }
+]);
