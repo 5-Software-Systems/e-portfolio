3af875d3a4be2b0b520523872a2cb947f4d7837c
flangton <flangton@student.unimelb.edu.au>
2020-09-30 14:42:00 +1000

    configs

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-import logging
 import pytest
 
 from flask import Flask, render_template
-from dotenv import load_dotenv
 
+from config import config
 from api import blueprint, db, flask_bcrypt
 from api.main.util.funcs import rel_path
-load_dotenv()
 
 
-def create_app(conf_file):
+def create_app():
     app = Flask(
         import_name='__main__',
         static_folder=rel_path('app/build', __file__),
@@ -17,29 +15,19 @@ def create_app(conf_file):
         static_url_path='/'
     )
 
-    app.config.from_pyfile(conf_file)
+    app.config.from_object(config)
+    print(config)
     db.init_app(app)
     flask_bcrypt.init_app(app)
 
     app.register_blueprint(blueprint, url_prefix='/api')
 
+    @app.route('/')
+    def index():
+        return render_template('index.html', token='Hello World')
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return render_template('index.html', token='Hello World')
+
     return app
-
-
-app = create_app(rel_path('config.py', __file__))
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', token='Hello World')
-
-
-@app.errorhandler(404)
-def not_found(e):
-    return render_template('index.html', token='Hello World')
-
-
-if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
