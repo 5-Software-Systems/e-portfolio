dc1f0af7944a4dcb7e2af65b2d84ced0410a50cc
flangton <flangton@student.unimelb.edu.au>
2020-10-15 15:41:58 +1100

    working

--- api/main/controller/widget_controller.py
+++ api/main/controller/widget_controller.py
@@ -4,6 +4,7 @@ from flask_restplus import Resource, Namespace
 from ..service import widget_service, user_service, portfolio_service
 
 from . import api_model
+from ..util.decorator import login_token_required
 
 namespace = Namespace(
     name='widget',
@@ -12,18 +13,22 @@ namespace = Namespace(
 )
 
 
-@namespace.route('/portfolio/<public_id>/widget')
-@namespace.param('public_id', 'The Portfolio identifier')
+@namespace.route('/user/<user_public_id>/portfolio/<portfolio_public_id>/widget')
+@namespace.param('user_public_id', 'The User identifier')
+@namespace.param('portfolio_public_id', 'The Portfolio identifier')
 class PortfolioWidget(Resource):
 
+    @namespace.expect(api_model.auth_token_header, validate=True)
     @namespace.marshal_with(api_model.widget_list, as_list=True, envelope='widgets')
-    def get(self, public_id):
+    @login_token_required
+    def get(self, user_public_id, portfolio_public_id):
         """List all Portfolio's Widgets"""
-        widgets = widget_service.get_all_portfolio_widgets(public_id)
+        widgets = widget_service.get_all_portfolio_widgets(portfolio_public_id)
         return [w.marshal() for w in widgets], 200
 
-    @namespace.expect(api_model.widget_new, validate=True)
+    @namespace.expect(api_model.widget_new, api_model.auth_token_header, validate=True)
     @namespace.marshal_with(api_model.widget, envelope='widget')
+    @login_token_required
     def post(self, public_id):
         """Create a new Widget"""
         data = request.json
@@ -33,28 +38,34 @@ class PortfolioWidget(Resource):
         return widget.marshal(), 201
 
 
-@namespace.route('/widget/<public_id>')
-@namespace.param('public_id', 'The Widget identifier')
+@namespace.route('/user/<user_public_id>/widget/<widget_public_id>')
+@namespace.param('user_public_id', 'The User identifier')
+@namespace.param('portfolio_public_id', 'The Portfolio identifier')
 class Widget(Resource):
 
+    @namespace.expect(api_model.auth_token_header, validate=True)
     @namespace.marshal_with(api_model.widget, envelope='widget')
-    def get(self, public_id):
+    @login_token_required
+    def get(self, user_public_id, widget_public_id):
         """Get a Widget"""
-        widget = widget_service.get_a_widget(public_id)
+        widget = widget_service.get_a_widget(widget_public_id)
         return widget.marshal(), 200
 
-    @namespace.expect(api_model.widget_update, validate=True)
+    @namespace.expect(api_model.widget_update, api_model.auth_token_header, validate=True)
     @namespace.marshal_with(api_model.widget, envelope='widget')
-    def patch(self, public_id):
+    @login_token_required
+    def patch(self, user_public_id, widget_public_id):
         """Update a Widget"""
         data = request.json
-        widget = widget_service.update_a_widget(public_id=public_id, data=data)
+        widget = widget_service.update_a_widget(public_id=widget_public_id, data=data)
         return widget.marshal(), 200
 
+    @namespace.expect(api_model.auth_token_header, validate=True)
     @namespace.marshal_with(api_model.response)
-    def delete(self, public_id):
+    @login_token_required
+    def delete(self, user_public_id, widget_public_id):
         """delete a Widget"""
-        res = widget_service.delete_a_widget(public_id)
+        res = widget_service.delete_a_widget(widget_public_id)
         return res, 200
 
 
