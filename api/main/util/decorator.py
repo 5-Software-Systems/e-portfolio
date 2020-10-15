431f238c4a8314f1db5615678d40fa39df417d75
flangton <flangton@student.unimelb.edu.au>
2020-10-15 22:27:20 +1100

    added comments

--- api/main/util/decorator.py
+++ api/main/util/decorator.py
@@ -5,14 +5,19 @@ from ..service.auth_service import decode_token, split_bearer_token
 from ..util.exception import AuthenticationError, Forbidden, RequestError
 
 
-def token_required(key, types):
+def token_required(key, permissions):
+    """
+    :param key: the key for the resource being protected eg. user, portfolio
+    :param permissions: list of permissions eg. login, reset, verify
+    :return:
+    """
     def token_required(f):
         @wraps(f)
         def decorated(*args, **kwargs):
             token = split_bearer_token(request.headers.get('Authorization'))
             payload = decode_token(token)
 
-            if not payload.get('type') in types:
+            if not payload.get('type') in permissions:
                 raise AuthenticationError
 
             token_id = payload.get(key)
