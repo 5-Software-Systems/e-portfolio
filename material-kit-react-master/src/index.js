249e8035b8b99d2a1c89ac457f11131e9da85393
Fabrice <fwongkwok@student.unimelb.edu.au>
2020-10-17 09:22:13 -0400

    still testing some shite

--- material-kit-react-master/src/index.js
+++ material-kit-react-master/src/index.js
@@ -1,26 +1,114 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import { isLoggedIn } from "./util/cookies";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import {
+    HomePage,
+    LoginPage,
+    SignUpPage,
+    ForgotPage,
+    ResetPage,
+    ProfilePage,
+    SettingsPage,
+    PortfolioPage,
+    UpdatesPage,
+    ContactUsPage,
+    Demo,
+    _404Page
+} from './routing.js';
 
-import "assets/scss/material-kit-react.scss?v=1.9.0";
-
-// pages for this product
-import Components from "views/Components/Components.js";
-import LandingPage from "views/LandingPage/LandingPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
-
-var hist = createBrowserHistory();
-
+//Routing
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/" component={Components} />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <Router>
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    component={HomePage}
+                />
+                <Route
+                    exact
+                    path="/login"
+                    render={() => {
+                        return (isLoggedIn() ? <Redirect to="/profile" /> : <LoginPage />)
+                    }}
+                />
+                <Route
+                    exact
+                    path="/signup"
+                    render={() => {
+                        return (isLoggedIn() ? <Redirect to="/profile" /> : <SignUpPage />)
+                    }}
+                />
+                <Route
+                    exact
+                    path="/forgot"
+                    render={() => {
+                        return (isLoggedIn() ? <Redirect to="/profile" /> : <ForgotPage />)
+                    }}
+                />
+                <Route
+                    exact
+                    path="/password_reset"
+                    render={() => {
+                        return (isLoggedIn() ? <Redirect to="/profile" /> : <ResetPage />)
+                    }}
+                />
+                <Route
+                    exact
+                    path="/profile"
+                    render={() => {
+                        return (isLoggedIn() ? <ProfilePage /> : <Redirect to="/" />)
+                    }}
+                />
+                <Route
+                    exact
+                    path="/settings"
+                    render={() => {
+                        return (isLoggedIn() ? <SettingsPage /> : <Redirect to="/" />)
+                    }}
+                />
+                <Route
+                    path="/portfolio/"
+                    render={() => {
+                        return (
+                                <PortfolioPage preview = {!isLoggedIn()} />
+                                )
+                    }}
+                />
+                <Route
+                    exact
+                    path="/demo"
+                    component={Demo}
+                />
+                <Route
+                    exact
+                    path="/updates"
+                    component={UpdatesPage}
+                />
+                <Route
+                    exact
+                    path="/contact"
+                    component={ContactUsPage}
+                />
+                <Route
+                    path="/"
+                    component={_404Page}
+                />
+            </Switch>
+        </Router>
+    </React.StrictMode>,
+  document.getElementById('content')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
