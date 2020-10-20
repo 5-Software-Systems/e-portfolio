6fef82283b45bf86a1d86e6bfde578a6f675dfa0
flangton <flangton@student.unimelb.edu.au>
2020-10-20 15:49:03 +1100

    fixes

--- api/main/service/file_service.py
+++ api/main/service/file_service.py
@@ -14,19 +14,41 @@ def get_user_files(user_public_id):
     return files
 
 
-def save_file(user_public_id: str, file_name: str, file_binary: bytes):
+def get_a_file(user_public_id: str, file_name: str):
     user = user_service.get_a_user(user_public_id)
-
-    file = File(user_id=user.id, file_name=file_name, file_binary=file_binary)
-    save_db_object(file)
-    return 'created'
-
-
-def get_file(public_id: str, file_name: str):
-    user = user_service.get_a_user(public_id)
 
     file = File.query.filter_by(user_id=user.id, file_name=file_name).first()
     if not file:
         raise FileNotFound(file_name)
+    return file
+
+
+def save_file(user_public_id: str, file_name: str, file_binary: bytes):
+    user = user_service.get_a_user(user_public_id)
+
+    file = File.query.filter_by(user_id=user.id, file_name=file_name).first()
+    if file:
+        file.delete()
+
+    file = File(user_id=user.id, file_name=file_name, file_binary=file_binary)
+    save_db_object(file)
+    return {
+        'status': 'success',
+        'message': 'file saved'
+    }
+
+
+def get_file_bytes(user_public_id: str, file_name: str):
+    file = get_a_file(user_public_id, file_name)
 
     return send_file(BytesIO(file.file_binary), as_attachment=True, attachment_filename=file.file_name)
+
+
+def delete_a_file(user_public_id: str, file_name: str):
+    file = get_a_file(user_public_id, file_name)
+    file.delete()
+
+    return {
+        'status': 'success',
+        'message': 'file deleted'
+    }
