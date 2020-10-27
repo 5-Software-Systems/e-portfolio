4c085d839bececb2b95404f3ea1cda85f5d82718
Nathan <20409659+NDoolan360@users.noreply.github.com>
2020-10-27 13:09:56 +1100

    removed Test component

--- app/src/templates/BaseTemplate.js
+++ app/src/templates/BaseTemplate.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react';
-import Test from '../util/Test';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
