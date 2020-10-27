324f11e80ff032a0a68342d6042237f91b393461
flangton <flangton@student.unimelb.edu.au>
2020-10-27 13:04:51 +1100

    added db.reflect()

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ flask_app = create_app()
 @pytest.fixture
 def app():
     with flask_app.app_context():
+        db.reflect()  # https://github.com/sqlalchemy/sqlalchemy/wiki/DropEverything
         db.drop_all()
         db.create_all()
     yield flask_app
